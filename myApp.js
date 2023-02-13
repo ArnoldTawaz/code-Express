e814961c389892d49a191f843479a51104769f09
ArnoldTawaz <38756515+ArnoldTawaz@users.noreply.github.com>
2023-02-13 00:40:53 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -7,8 +7,9 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
   res.json({
-    message: "Hello json"
+    message: "Hello json".toUpperCase()
   });
+  process.env.MESSAGE_STYLE;
 });
 console.log("Hello World");
 
