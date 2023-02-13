e9e5c8b12f1966ff4e72eadc1ed56e4cdd3fd2a1
ArnoldTawaz <38756515+ArnoldTawaz@users.noreply.github.com>
2023-02-13 00:36:15 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,16 @@
 let express = require('express');
 let app = express();
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
+});
+console.log("Hello World");
 
 
 
